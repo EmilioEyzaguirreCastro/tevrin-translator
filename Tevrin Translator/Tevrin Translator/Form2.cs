742ffc12d55e12e124f54398d1431bed20dbe775
Emilio <63172577+EmilioEyzaguirreCastro@users.noreply.github.com>
2025-07-12 18:27:44 -0500

    Add files via upload
    thing

---
+++ Tevrin Translator/Tevrin Translator/Form2.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tevrin_Translator
+{
+    public partial class Form2 : Form
+    {
+        private Form1 mainForm;
+        public Form2(Form1 form)
+        {
+            InitializeComponent();
+            this.mainForm = form;
+        }
+
+        private void Form2_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            mainForm.aboutUs.Enabled = true;
+            mainForm.submitEnglish.Enabled = true;
+            // mainForm.submitSpanish.Enabled = true;
+            mainForm.textBox1.ReadOnly = false;
+        }
+    }
+}
---
+++ Tevrin Translator/Tevrin Translator/OllamaClient.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+using System.Net.Http;
+
+
+namespace Tevrin_Translator
+{
+    public class OllamaClient
+    {
+        // Cliente http
+        private static readonly HttpClient httpClient = new HttpClient();
+
+        // Funcion del request
+        public async Task<string> Request(string prompt)
+        {
+            var requestBody = new
+            {
+                model = "mistral",
+                messages = new[]
+                {
+                    new { role = "user", content = prompt }
+                },
+
+                stream = false
+            };
+
+            string json = JsonSerializer.Serialize(requestBody);
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+            var response = await httpClient.PostAsync("http://localhost:11434/api/chat", content);
+            string result = await response.Content.ReadAsStringAsync();
+
+            using var doc = JsonDocument.Parse(result);
+            string answer = doc.RootElement
+                .GetProperty("message")
+                .GetProperty("content")
+                .GetString();
+
+            return answer;
+
+        }
+    }
+}
